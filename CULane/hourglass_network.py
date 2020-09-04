73bf4bcf2ca79c35247284185701697a63b82145
koyeongmin <60723886+koyeongmin@users.noreply.github.com>
2020-09-04 12:14:34 +0900

    Update hourglass_network.py

--- CULane/hourglass_network.py
+++ CULane/hourglass_network.py
@@ -21,8 +21,8 @@ class lane_detection_network(nn.Module):
         #feature extraction
         self.layer1 = hourglass_block(128, 128)
         self.layer2 = hourglass_block(128, 128)
-        #self.layer3 = hourglass_block(128, 128)
-        #self.layer4 = hourglass_block(128, 128)
+        self.layer3 = hourglass_block(128, 128)
+        self.layer4 = hourglass_block(128, 128)
 
 
     def forward(self, inputs):
@@ -30,8 +30,8 @@ class lane_detection_network(nn.Module):
         out = self.resizing(inputs)
         result1, out, feature1 = self.layer1(out)
         result2, out, feature2 = self.layer2(out)   
-        #result3, out, feature3 = self.layer3(out)
-        #result4, out, feature4 = self.layer4(out)
+        result3, out, feature3 = self.layer3(out)
+        result4, out, feature4 = self.layer4(out)
 
-        #return [result1, result2, result3, result4], [feature1, feature2, feature3, feature4]
-        return [result2], [feature2]
+        return [result1, result2, result3, result4], [feature1, feature2, feature3, feature4]
+        #return [result2], [feature2]
